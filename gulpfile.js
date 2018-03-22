7c8ea9c2b25be3b7d070beccb615bd3e551d9fcc
AlexWang <wangqiwenboy@hotmail.com>
2018-03-22 19:17:49 +0800

    更改文档内路径

--- gulpfile.js
+++ gulpfile.js
@@ -34,39 +34,39 @@ var gulp = require('gulp'),
     Optimize the file
 ------------------------- */
 
-    //为css下文件添加厂商前缀，从/src/css 到 dist/js输出两个文件 .min.css & .js
+    //为css下文件添加厂商前缀，从/src/css 到 docs/js输出两个文件 .min.css & .js
     gulp.task('styles', function() {
         return gulp.src('src/css/main.css')
             .pipe(autoprefixer('last 2 version'))
-            .pipe(gulp.dest('dist/css'))
+            .pipe(gulp.dest('docs/css'))
             .pipe(rename({suffix: '.min'}))
             .pipe(cleanCss())
-            .pipe(gulp.dest('dist/css'))
+            .pipe(gulp.dest('docs/css'))
             .pipe(notify({ message: 'Styles task complete' }));
     });
 
-    //从src/js 到 dist/js输出两个文件.min.js & .js
+    //从src/js 到 docs/js输出两个文件.min.js & .js
     gulp.task('scripts', function() {
         //创建过滤器实例
         // var f = filter(['**', '!src/*min.js'], {restore: true});
         return gulp.src('src/js/*.js')
             .pipe(cached())
-            .pipe(gulp.dest('dist/js'))
+            .pipe(gulp.dest('docs/js'))
             .pipe(rename({suffix: '.min'}))
             .pipe(uglify())
-            .pipe(gulp.dest('dist/js'))
+            .pipe(gulp.dest('docs/js'))
             .pipe(notify({ message: 'Script task complete'}));
     });
 
-    //以默认参数优化，从 src/images 到 dist/images
+    //以默认参数优化，从 src/images 到 docs/images
     gulp.task('images', function() {
         return gulp.src('src/images/**/*')
             .pipe(cached(imagemin({verbose: true})))
-            .pipe(gulp.dest('dist/images'))
+            .pipe(gulp.dest('docs/images'))
             .pipe(notify({ message: 'Images task complete'}));
     });
     
-    //按html载入顺序合并.js文件，输出到 dist/vendor
+    //按html载入顺序合并.js文件，输出到 docs/vendor
     gulp.task('usemerger', function() {
         var s = size();
         
@@ -76,7 +76,7 @@ var gulp = require('gulp'),
         .pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
         //需要压缩，提取.js
         // .pipe(gulpif('*.js', uglify()))
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('docs/'));
     });
     
     //综合运行任务
@@ -90,32 +90,32 @@ var gulp = require('gulp'),
 
     //清理文件
     gulp.task('clean', function() {
-        return del('dist/');
+        return del('docs/');
     });
     
 /* --------------------------
         Move the file
 ------------------------- */
 
-    //复制fonts/,pdf/,vendor/ -> dist/
+    //复制fonts/,pdf/,vendor/ -> docs/
     gulp.task('copy', function() {
         gulp.src('src/fonts/*')
-        .pipe(gulp.dest('dist/fonts/')),
+        .pipe(gulp.dest('docs/fonts/')),
 
         gulp.src('src/pdf/*')
-        .pipe(gulp.dest('dist/pdf/'))
+        .pipe(gulp.dest('docs/pdf/'))
 
         gulp.src('src/vendor/**/*')
-        .pipe(gulp.dest('dist/vendor'))
+        .pipe(gulp.dest('docs/vendor'))
     })
 
     //提取插件文件从/node_modules 到 /vendor
     gulp.task('export', function() {
         return merge(
-            gulp.src(['node_modules/bootstrap/dist/**/*', '!**/*theme.*', '!**/*.map', '!**/npm.js'])
+            gulp.src(['node_modules/bootstrap/docs/**/*', '!**/*theme.*', '!**/*.map', '!**/npm.js'])
             .pipe(gulp.dest('src/vendor/bootstrap')),
             
-            gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+            gulp.src(['node_modules/jquery/docs/jquery.js', 'node_modules/jquery/docs/jquery.min.js'])
             .pipe(gulp.dest('src/vendor/jquery')),
             
             gulp.src('node_modules/jquery.localscroll/*.js')
@@ -124,7 +124,7 @@ var gulp = require('gulp'),
             gulp.src('node_modules/jquery.scrollto/*.js')
             .pipe(gulp.dest('src/vendor/jquery.scrollto')),
             
-            gulp.src('node_modules/scrollreveal/dist/*.js')
+            gulp.src('node_modules/scrollreveal/docs/*.js')
             .pipe(gulp.dest('src/vendor/scrollreveal')),
             
             gulp.src([
